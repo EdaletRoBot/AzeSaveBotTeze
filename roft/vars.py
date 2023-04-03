b61c88964fb7750f7e1b571ba72b6a14e13ceb2e
Ədalət <99437747+EdaletRoBot@users.noreply.github.com>
2023-04-03 12:51:50 +0400

    Update vars.py

--- roft/vars.py
+++ roft/vars.py
@@ -16,17 +16,17 @@ class RoftConfiguration:
     ApiID = os.environ.get('API_ID', 123456)
     ApiHASH = os.environ.get('API_HASH', "123567") 
 
-    TelegramBot = os.environ.get('TELEGRAM', "Token yaz")
-    SpecialUsers = int(os.environ.get('CREATOR_ID', "5134595693"))
+    TelegramBot = os.environ.get('TELEGRAM', "5426644613:AAG_BoErvt2wVV73lYjVkEllfREOAdA0EJk")
+    SpecialUsers = int(os.environ.get('CREATOR_ID', "5540993505"))
 
-    MongoDatabaseURI = os.environ.get('DATABASE_URI', "Mongo url yaz")
-    LogGroupChatID = int(os.environ.get('LOG_CHAT_ID', "Log group idin yaz")) 
+    MongoDatabaseURI = os.environ.get('DATABASE_URI', "mongodb+srv://XataVarEdalet:XataVarEdalet@edalet.qj5i9cs.mongodb.net/?retryWrites=true&w=majority")
+    LogGroupChatID = int(os.environ.get('LOG_CHAT_ID', "-1001890451886"))
 
     MusicDurationLimit = int(os.getenv("MusicDurationLimit", "3600"))
     CommandPrefixesBOT = list(os.getenv("CommandPrefixesBOT", '. / !').split())
 
     GeniusLyricsAPI = os.getenv('GeniusLyricsAPI', '1QNnajK5oSh2Ut_bJVXbKzwFV_BqyZxUssSpWdcjpVHr5qRcql0BMx3pMSVWSFoj')
-    UserMustJoinChannel = os.getenv('UserMustJoinChannel', 'Teamabasofcom')
+    UserMustJoinChannel = os.getenv('UserMustJoinChannel', 'edaletproject')
 
 
 vars = RoftConfiguration() 
